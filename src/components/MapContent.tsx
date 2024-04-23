1f7f2b572a30520b051ef038778f768995fabf93
siwat jermwatthana <siwat.j@hiveground.com>
2024-04-23 17:48:28 +0700

    Feat: Add Polygon component to MapContent

--- src/components/MapContent.tsx
+++ src/components/MapContent.tsx
@@ -1,18 +1,26 @@
 "use client";
 
 import React from "react";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, Polygon, TileLayer, useMap } from "react-leaflet";
 import LayerControl from "./layers/LayerControl";
 import MarkerFromCSV from "./files/MarkerFromCSV";
+import { LatLngExpression } from "leaflet";
 
 type Props = {};
 
 const MapContent = (props: Props) => {
+  const polygon: LatLngExpression[] = [
+    [51.515, -0.09],
+    [51.52, -0.1],
+    [51.52, -0.12],
+  ];
+
   return (
     <div>
       <MapContainer style={{ width: "100%", height: "100vh" }} center={[13, 100]} zoom={5} scrollWheelZoom={true}>
         <LayerControl />
         <MarkerFromCSV />
+        <Polygon positions={polygon} />
       </MapContainer>
     </div>
   );
